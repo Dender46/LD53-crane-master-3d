44cac33f0f6bcb88ba087db5d63967a0119dfb03
Dender46 <dmitriy.bagandov@gmail.com>
2023-04-30 12:06:52 +0300

    Make grabbed box upon releasing follow natural velocity

--- Assets/Grappler.cs
+++ Assets/Grappler.cs
@@ -15,6 +15,16 @@ public class Grappler : MonoBehaviour
         {
             _MagnetizedBox.transform.position = transform.position;
             _MagnetizedBox.transform.rotation = transform.rotation;
+
+            var boxRB = _MagnetizedBox.GetComponent<Rigidbody>();
+            var grapplerRB = transform.parent.GetComponent<Rigidbody>();
+            boxRB.velocity = grapplerRB.velocity;
+            boxRB.angularVelocity = grapplerRB.angularVelocity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _MagnetizedBox = null;
         }
     }
 
