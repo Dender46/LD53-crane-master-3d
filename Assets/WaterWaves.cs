8fc576e73174bf782847eb94169e081e5c85af37
Dender46 <dmitriy.bagandov@gmail.com>
2023-05-01 20:39:47 +0300

    Offset water texture over time

--- Assets/WaterWaves.cs
+++ Assets/WaterWaves.cs
@@ -17,5 +17,10 @@ public class WaterWaves : MonoBehaviour
 
         _WaterPlane.position = new Vector3(_WaterPlane.position.x, _WaterPlane.position.y + _Wobble, _WaterPlane.position.z);
         transform.position = new Vector3(transform.position.x, transform.position.y + _Wobble, transform.position.z);
+
+        var textureOffset = _WaterMaterial.mainTextureOffset;
+        textureOffset.x -= _Speed * Time.deltaTime;
+        textureOffset.y -= _Speed * Time.deltaTime;
+        _WaterMaterial.mainTextureOffset = textureOffset;
     }
 }
