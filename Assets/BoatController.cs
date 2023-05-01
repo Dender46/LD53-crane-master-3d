c8a904140be6902ce88108355bbbd99658553c46
Dender46 <dmitriy.bagandov@gmail.com>
2023-05-01 16:06:43 +0300

    Add UI for boat missions

--- Assets/BoatController.cs
+++ Assets/BoatController.cs
@@ -1,14 +1,32 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoatController : MonoBehaviour
 {
+    [SerializeField] Text _UIMissionText;
+    [Space(10)]
     [SerializeField] Transform _BoxesDeliveryContainer;
     [SerializeField] float _Speed = 5.0f;
 
     [ReadOnly] public int _NeedBoxes = 3;
     [ReadOnly] public int _BoxesOnBoard = 0;
 
+    void Start()
+    {
+        _UIMissionText.text = "0 / " + _NeedBoxes;
+    }
+
+    void Update()
+    {
+        _UIMissionText.rectTransform.parent.position = Camera.main.WorldToScreenPoint(transform.position);
+        if (transform.position.z > 5)
+        {
+            var newScale = _UIMissionText.rectTransform.parent.localScale;
+            newScale.x -= 0.3f * Time.deltaTime;
+            newScale.y -= 0.3f * Time.deltaTime;
+            _UIMissionText.rectTransform.parent.localScale = newScale;
+        }
+    }
 
     public void Move()
     {
@@ -33,7 +51,9 @@ public class BoatController : MonoBehaviour
         {
             other.transform.parent = _BoxesDeliveryContainer;
             GameManager.Score++;
+
             _BoxesOnBoard++;
+            _UIMissionText.text = _BoxesOnBoard + " / " + _NeedBoxes;
 
             if (_BoxesOnBoard >= _NeedBoxes)
             {
